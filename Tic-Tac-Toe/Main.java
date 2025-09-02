a44f0c71c0ea4b3f1262bc45f649626c63ccb913
Tansi Ajit <tansiajit@gmail.com>
2025-09-02 11:41:08 +0400

    Creating the Grid

--- Tic-Tac-Toe/Main.java
+++ Tic-Tac-Toe/Main.java
@@ -1,2 +1,16 @@
-import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        char[][] grid = {
+            { 'X', 'O', 'X' },
+            { 'O', 'X', 'O' },
+            { 'X', 'X', 'O' }
+        };
 
+        for (int i = 0; i<3; i++) {
+            for(int j = 0; j<3; j++) {
+                System.out.print(grid[i][j]+" ");
+            }
+            System.out.println("");
+        }
+    }
+}
